2b7a4d67cb097080fb734e4a72e5bef2a13ef29a
Hugo_ <55756021+Dhaiven@users.noreply.github.com>
2025-11-20 11:55:18 +0100

    Add fichiers exercices

--- fichiers/08-lecture-donnees-main/main.py
+++ fichiers/08-lecture-donnees-main/main.py
@@ -14,38 +14,35 @@ def read_data(filename):
         list: le contenu du fichier (1 list par ligne)
     """
     l = []
+    with open(filename) as f:
+        for line in f:
+            l.append([int(value) for value in line.split(";")])
     return l
 
 def get_list_k(data, k):
-    l = []
-    return l
+    return data[k]
 
 def get_first(l):
-    return None
+    return l[0]
 
 def get_last(l):
-    return None
+    return l[-1]
 
 def get_max(l):
-    return None
+    return max(l)
 
 def get_min(l):
-    return None
+    return min(l)
 
 def get_sum(l):
-    return None
-
-
-#### Fonction principale
-
+    return sum(l)
 
 def main():
-    pass
-    # data = read_data(FILENAME)
-    # for i, l in enumerate(data):
-    #     print(i, l)
-    # k = 37
-    # print(k, get_list_k(data, 37))
+    data = read_data(FILENAME)
+    for i, l in enumerate(data):
+        print(i, l)
+    k = 37
+    print(k, get_list_k(data, 37))
 
 
 if __name__ == "__main__":
